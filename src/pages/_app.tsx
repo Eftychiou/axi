af608bc01f08b9e13d5f02c7b9f714ff137a531b
Giorgos Eftichiou <eftichiou@hotmail.com>
2024-07-15 11:44:04 +0300

    StartProcessing Counter triggers loop

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Axi Bank Counter assessment</title>
+        <meta name="description" content="Axi Bank Counter assessment" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />;
+    </>
+  );
 }
